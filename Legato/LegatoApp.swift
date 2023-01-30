8900de8dfd19a44b2611b2a8bc5b2192470d265a
anthonymarie22 <72616953+anthonymarie22@users.noreply.github.com>
2023-01-29 22:12:14 -0500

    Initial Commit

---
+++ Legato/LegatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegatoApp.swift
+//  Legato
+//
+//  Created by Anthony Marie on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LegatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
